eb4842aa011b9ecfc40c9f004de88ca0f7f693b8
Geladeira95 <levi.rodrigues6@etec.sp.gov.br>
2024-09-30 18:22:51 -0300

    Add files via upload

---
+++ loading.js
@@ -0,0 +1,6 @@
+
+let I = function () {
+document.querySelector(".loader").style.display = "none"
+};
+
+I();
---
+++ script.js
@@ -0,0 +1,39 @@
+
+
+
+function toggleSidebar() {
+  var sidebar = document.getElementById("div_menu");
+  if (sidebar.style.width === "250px") {
+    sidebar.style.width = "0";
+  } else {
+    sidebar.style.width = "250px";
+  }
+}
+
+
+
+//carrossel
+const carousel = document.querySelector('.carroseu');
+const cards = document.querySelectorAll('.carta');
+const prevBtn = document.querySelector('.botao_volta');
+const nextBtn = document.querySelector('.botao_vai');
+
+
+let a = 0;
+
+function updateCarousel() {
+  carousel.style.transform = `translateX(${-a * 260}px)`;
+}
+
+nextBtn.addEventListener('click', () => {
+  a = (a + 1) % cards.length;
+  updateCarousel();
+});
+
+prevBtn.addEventListener('click', () => {
+  a = (a - 1 + cards.length) % cards.length;
+  updateCarousel();
+});
+
+
+ 
